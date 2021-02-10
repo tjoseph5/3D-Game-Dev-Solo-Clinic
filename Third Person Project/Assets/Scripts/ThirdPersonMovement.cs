8500ff8b26d1d85df30b3a0220a96932aa98c25d
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-02-10 05:04:31 -0500

    Added refined model with UV

--- Third Person Project/Assets/Scripts/ThirdPersonMovement.cs
+++ Third Person Project/Assets/Scripts/ThirdPersonMovement.cs
@@ -10,12 +10,30 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public float speed;
 
+    Vector3 velocity;
+    public float gravity;
+    public float jumpHeight;
+
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -29,5 +47,14 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
